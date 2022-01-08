0350773cec5451ed7ae169efb460318ca35a8be4
yoojin-park19 <yoojin3972@gmail.com>
2022-01-08 20:39:32 +0900

    creat, read 구현

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const nunjucks = require('nunjucks');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const foods= require('./database/food')
+const blogRouter = require('./router/blog.js');
+const dateFilter = require('nunjucks-date-filter');
+const path = require('path');
+
+
+app.set('view engine', 'html');
+
+경로 = path.join(path.join(__dirname + '/resource', '/static'));
+console.log(경로);
+
+app.use('/', express.static(경로));
+app.use('/blog', express.static(경로));
+
+let env = nunjucks.configure('resource/template', {
+    autoescape: true,
+    express: app,
+    watch: true
+});
+env.addFilter('date', dateFilter);
+
+app.use(express.json());
+app.use(express.urlencoded( {extended : false } ));
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('tiny'));
+
+app.get('/', (req, res, next) => {
+    res.render('index.html');
+});
+app.use('/blog', blogRouter);
+
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+})
+
+app.use((err, req, res, next) => {
+    console.log('애러났음!')
+    console.log(err);
+    res.sendStatus(500);
+})
+app.listen(8080);
---
+++ database/food.js
@@ -0,0 +1,117 @@
+
+
+let foods = [{
+    id : 1,
+    section: '성산',
+    name : '해왓',
+    content: '각종 생선요리 전문점 모든 메뉴가 평타이상',
+    img: '/assets/해왓.jpeg',
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+},{
+    id : 2,
+    section: '성산',
+    name : '지환이네 수산물',
+    content: '강추! 바다가 바로앞에 위치한 해산물포차',
+    img: '/assets/지환이네수산물.jpeg',
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+},{
+    id : 3,
+    section: '성산',
+    name : '해녀의 밥상',
+    content: '해녀가 직접 딴 제주해산물 풀코스',
+    img: '/assets/해녀의밥상.jpeg',
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+},{
+    id : 4,
+    section: '성산',
+    name : '성산 흑돼지 두루치기',
+    content: '제주 흑돼지구이 질렸으면 두루치기 추천! 몸국강추!',
+    img: '/assets/성산흑돼지두루치기.jpeg',
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+},{
+    id : 5,
+    section: '성산',
+    name : '호야네 집밥',
+    content: '제주 집밥 느낌의 소박한 가게',
+    img: '/assets/호야네집밥.jpeg',
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+},{
+    id : 6,
+    section: '표선',
+    name : '표선우동가게',
+    content: '일본인이 추천하는 우동 맛집',
+    img: '/assets/표선우동가게.jpeg',
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+},{
+    id : 7,
+    section: '표선',
+    name : '오늘은 녹차한잔',
+    content: '힐링이 되는 녹차밭 까페',
+    img: '/assets/오늘은녹차한잔.jpeg',
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+},{
+    id : 8,
+    section: '중문',
+    name : '대기정',
+    content: '가족끼리 편히 먹을 수 있는 갈치전문점',
+    img: '/assets/대기정.jpeg',
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+},{
+    id : 9,
+    section: '중문',
+    name : '중문해녀의 집',
+    content: '찐 해녀분들의 해산물 맛집',
+    img: '/assets/중문해녀의집.jpeg',
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+},{
+    id : 10,
+    section: '안덕',
+    name : '해월정',
+    content: '맛있는 녀석들 보말칼국수',
+    img: '/assets/해월정.jpeg',
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+},{
+    id : 10,
+    section: '안덕',
+    name : '산방산나들목',
+    content: '제주도민 방문 많은 생선구이',
+    img: '/assets/산방산나들목.jpeg',
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+},{
+    id : 10,
+    section: '안덕',
+    name : '해월정',
+    content: '맛있는 녀석들 보말칼국수',
+    img: '/assets/해월정.jpeg',
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+},{
+    id : 10,
+    section: '안덕',
+    name : '원앤온리원',
+    content: '인스타에서 유명한 인생샷 까페',
+    img: '/assets/원앤온리.jpeg',
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+},{
+    id : 10,
+    section: '안덕',
+    name : '해이브라더',
+    content: '사계해변이 바로보이는 까페',
+    img: '/assets/헤이브라더.jpeg',
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+}];
+
+module.exports = foods;
