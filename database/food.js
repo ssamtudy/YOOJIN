ee3b3c6b7e8fe7c5f141d505d103f85023cf8d84
yoojin-park19 <yoojin3972@gmail.com>
2022-01-15 22:54:16 +0900

    css완성

--- database/food.js
+++ database/food.js
@@ -1,117 +1,202 @@
-
-
-let foods = [{
-    id : 1,
+let foods = [
+  {
+    id: 1,
     section: '성산',
-    name : '해왓',
+    name: '해왓',
     content: '각종 생선요리 전문점 모든 메뉴가 평타이상',
     img: '/assets/해왓.jpeg',
-    pubDate : new Date().toString(),
-    modDate : new Date().toString()
-},{
-    id : 2,
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 2,
     section: '성산',
-    name : '지환이네 수산물',
+    name: '지환이네 수산물',
     content: '강추! 바다가 바로앞에 위치한 해산물포차',
     img: '/assets/지환이네수산물.jpeg',
-    pubDate : new Date().toString(),
-    modDate : new Date().toString()
-},{
-    id : 3,
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 3,
     section: '성산',
-    name : '해녀의 밥상',
+    name: '해녀의 밥상',
     content: '해녀가 직접 딴 제주해산물 풀코스',
     img: '/assets/해녀의밥상.jpeg',
-    pubDate : new Date().toString(),
-    modDate : new Date().toString()
-},{
-    id : 4,
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 4,
     section: '성산',
-    name : '성산 흑돼지 두루치기',
+    name: '성산 흑돼지 두루치기',
     content: '제주 흑돼지구이 질렸으면 두루치기 추천! 몸국강추!',
     img: '/assets/성산흑돼지두루치기.jpeg',
-    pubDate : new Date().toString(),
-    modDate : new Date().toString()
-},{
-    id : 5,
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 5,
     section: '성산',
-    name : '호야네 집밥',
+    name: '호야네 집밥',
     content: '제주 집밥 느낌의 소박한 가게',
     img: '/assets/호야네집밥.jpeg',
-    pubDate : new Date().toString(),
-    modDate : new Date().toString()
-},{
-    id : 6,
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 6,
     section: '표선',
-    name : '표선우동가게',
+    name: '표선우동가게',
     content: '일본인이 추천하는 우동 맛집',
     img: '/assets/표선우동가게.jpeg',
-    pubDate : new Date().toString(),
-    modDate : new Date().toString()
-},{
-    id : 7,
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 7,
     section: '표선',
-    name : '오늘은 녹차한잔',
+    name: '오늘은 녹차한잔',
     content: '힐링이 되는 녹차밭 까페',
     img: '/assets/오늘은녹차한잔.jpeg',
-    pubDate : new Date().toString(),
-    modDate : new Date().toString()
-},{
-    id : 8,
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 8,
     section: '중문',
-    name : '대기정',
+    name: '대기정',
     content: '가족끼리 편히 먹을 수 있는 갈치전문점',
     img: '/assets/대기정.jpeg',
-    pubDate : new Date().toString(),
-    modDate : new Date().toString()
-},{
-    id : 9,
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 9,
     section: '중문',
-    name : '중문해녀의 집',
+    name: '중문해녀의 집',
     content: '찐 해녀분들의 해산물 맛집',
     img: '/assets/중문해녀의집.jpeg',
-    pubDate : new Date().toString(),
-    modDate : new Date().toString()
-},{
-    id : 10,
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 10,
     section: '안덕',
-    name : '해월정',
+    name: '해월정',
     content: '맛있는 녀석들 보말칼국수',
     img: '/assets/해월정.jpeg',
-    pubDate : new Date().toString(),
-    modDate : new Date().toString()
-},{
-    id : 10,
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 11,
     section: '안덕',
-    name : '산방산나들목',
+    name: '산방산나들목',
     content: '제주도민 방문 많은 생선구이',
     img: '/assets/산방산나들목.jpeg',
-    pubDate : new Date().toString(),
-    modDate : new Date().toString()
-},{
-    id : 10,
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 12,
     section: '안덕',
-    name : '해월정',
-    content: '맛있는 녀석들 보말칼국수',
-    img: '/assets/해월정.jpeg',
-    pubDate : new Date().toString(),
-    modDate : new Date().toString()
-},{
-    id : 10,
-    section: '안덕',
-    name : '원앤온리원',
+    name: '원앤온리원',
     content: '인스타에서 유명한 인생샷 까페',
     img: '/assets/원앤온리.jpeg',
-    pubDate : new Date().toString(),
-    modDate : new Date().toString()
-},{
-    id : 10,
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 13,
     section: '안덕',
-    name : '해이브라더',
+    name: '해이브라더',
     content: '사계해변이 바로보이는 까페',
     img: '/assets/헤이브라더.jpeg',
-    pubDate : new Date().toString(),
-    modDate : new Date().toString()
-}];
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 14,
+    section: '한림',
+    name: '협재1번가',
+    content: '바닷가 바로앞 술집(분위기강추)',
+    img: '/assets/협재1번가.jpeg',
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 15,
+    section: '한림',
+    name: '수우동',
+    content: '바닷가 바로앞 우동집(나혼자 산다맛집)',
+    img: '/assets/수우동.jpeg',
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 16,
+    section: '한림',
+    name: '성아시',
+    content: '제주 집밥감성 뿜뿜 맛집',
+    img: '/assets/성아시.jpeg',
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 17,
+    section: '제주시',
+    name: '우진해장국',
+    content: '고사리육개장으로 유명한 맛집',
+    img: '/assets/우진해장국.jpeg',
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 18,
+    section: '제주시',
+    name: '마모루',
+    content: '시청근처 신선한 냉라멘 맛집',
+    img: '/assets/마모루.jpeg',
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 16,
+    section: '제주시',
+    name: '노형수산시장',
+    content: '노량진수산시장 느낌의 가성비 횟집',
+    img: '/assets/노형수산시장.jpeg',
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 19,
+    section: '제주시',
+    name: '제주돔베고기집',
+    content: '제주 보쌈김치 제일 맛있는 집',
+    img: '/assets/제주돔베고기집.jpeg',
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 20,
+    section: '제주시',
+    name: '칠돈가',
+    content: '연탄불로 구운 흑돼지 맛집',
+    img: '/assets/칠돈가.jpeg',
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+  {
+    id: 21,
+    section: '제주시',
+    name: 'Aran 9st',
+    content: '제주에서 혼술 분위기 좋은 와인바',
+    img: '/assets/아란9길.jpeg',
+    pubDate: new Date().toString(),
+    modDate: new Date().toString(),
+  },
+];
 
 module.exports = foods;
--- router/blog.js
+++ router/blog.js
@@ -1,81 +1,80 @@
 const express = require('express');
 const path = require('path');
 let foods = require('../database/food');
-const multer = require('multer')
+const multer = require('multer');
 const router = express.Router();
 
 경로 = path.join(path.join(__dirname + '/resource', '/static'));
 console.log(경로);
 router.use('/blog/', express.static(경로));
 
-
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.filename)
-    }
-})
-  
-const upload = multer({ storage: storage })
-router.post('/write', upload.single('img'), function (req, res, next) {
-})
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.filename);
+  },
+});
+
+const upload = multer({ storage: storage });
+router.post('/write', upload.single('img'), function (req, res, next) {});
 
 router.get('/', (req, res, next) => {
-    console.log(req.query);
-    const section = req.query.section;
-    let data = section ? foods.filter(b => b.section === section).slice(0,3) : foods;
-    res.render('post.html', {data});
-}); 
+  console.log(req.query);
+  const section = req.query.section;
+  let data = section
+    ? foods.filter((b) => b.section === section).slice(0, 3)
+    : foods.slice(0, 3);
+  res.render('post.html', { data });
+});
 router.get('/write', (req, res, next) => {
-    res.render('write.html');
+  res.render('write.html');
 });
 router.post('/', (req, res, next) => {
-    const id = foods.length + 1;
-    const section = req.body.section;
-    const name = req.body.name;
-    const content = req.body.content;
-    const img = req.body.img;
-    const pubDate = new Date().toString();
-    const modDate = new Date().toString();
-    let food = {id, section, name, content, img,  pubDate, modDate};
-    foods.push(food);
-    res.redirect('/blog/');
-})
+  const id = foods.length + 1;
+  const section = req.body.section;
+  const name = req.body.name;
+  const content = req.body.content;
+  const img = req.body.img;
+  const pubDate = new Date().toString();
+  const modDate = new Date().toString();
+  let food = { id, section, name, content, img, pubDate, modDate };
+  foods.push(food);
+  res.redirect('/blog/');
+});
 router.get('/', (req, res, next) => {
-    res.render('post.html', {data});
-}); 
+  res.render('post.html', { data });
+});
 
 router.get('/:id', (req, res, next) => {
-    let id = req.params.id;
-    let food = foods.find(b => b.id == id); 
-    console.log(food)
-    res.render('postdetail.html', {food});
+  let id = req.params.id;
+  let food = foods.find((b) => b.id == id);
+  console.log(food);
+  res.render('postdetail.html', { food });
 });
 
 router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    const food = foods.find(b => b.id == id)
-    if (food){ 
-        food.section = req.body.section;
-        food.name = req.body.name;
-        food.content = req.body.content;
-        food.img = req.body.img;
-        food.modDate = new Date().toString();
-        res.status(200).json(foods);
-    } else {
-        res.status(404);
-    }
-})
+  const id = req.params.id;
+  const food = foods.find((b) => b.id == id);
+  if (food) {
+    food.section = req.body.section;
+    food.name = req.body.name;
+    food.content = req.body.content;
+    food.img = req.body.img;
+    food.modDate = new Date().toString();
+    res.status(200).json(foods);
+  } else {
+    res.status(404);
+  }
+});
 
 // 삭제(DELETE) : blog/:id
 router.delete('/:id', (req, res, next) => {
-    const id = req.params.id;
-    foods = foods.filter(b => b.id != id) 
-    res.status(200).json(foods); 
-})
-
+  const id = req.params.id;
+  foods = foods.filter((b) => b.id != id);
+  res.status(200).json(foods);
+});
 
 module.exports = router;
 module.exports.foods = foods;
